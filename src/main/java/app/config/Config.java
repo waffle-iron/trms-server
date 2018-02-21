5c187f57a2ddbfb91bd37da3d6ac6efa379a1b28
Robert Hamel <roberthamel@gmail.com>
2018-02-21 08:57:58 -0500

    add .env

---
+++ src/main/java/app/config/Config.java
@@ -0,0 +1,51 @@
+package app.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Config {
+
+    private Dotenv dotEnv;
+
+    private String db;
+    private String username;
+    private String password;
+    private String testDb;
+    private String testdbusername;
+    private String testdbpassword;
+
+    public Config() {
+        dotEnv = Dotenv
+                .configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+    }
+
+    public static Config env() {
+        return new Config();
+    }
+
+    public String getDb() {
+        return dotEnv.get("DBURL");
+    }
+
+    public String getUsername() {
+        return dotEnv.get("DBUSERNAME");
+    }
+
+    public String getPassword() {
+        return dotEnv.get("DBPASSWORD");
+    }
+
+    public String getTestDb() {
+        return dotEnv.get("TESTDBURL");
+    }
+
+    public String getTestdbusername() {
+        return dotEnv.get("TESTDBUSERNAME");
+    }
+
+    public String getTestdbpassword() {
+        return dotEnv.get("TESTDBPASSWORD");
+    }
+}
