112b24a135b92a16d061aa89e41b0a01c94716cd
Robert Hamel <roberthamel@gmail.com>
2018-02-21 00:03:32 -0500

    add connection utility

---
+++ src/test/java/app/utilities/ConnectionUtilityTest.java
@@ -0,0 +1,17 @@
+package app.utilities;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ConnectionUtilityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
